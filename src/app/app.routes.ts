51b6ac72853a5a317326b973987664aa20412077
Nguyen Dong Ha <ndha@tma.com.vn>
2025-11-07 14:50:56 +0700

    add lazy load for admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,51 +1,24 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/admin/login/login.component';
-import { LayoutComponent } from './pages/admin/layout/layout.component';
-import { AuthGuard } from './guards/auth.guard';
-import { ProductsComponent } from './pages/admin/products/products.component';
 import { ProductsComponent as ProductsComponentPage } from './pages/website/products/products.component';
-import { CategoriesComponent } from './pages/admin/categories/categories.component';
 import { LandingComponent } from './pages/website/landing/landing.component';
 import { CategoryProductsComponent } from './pages/website/category-products/category-products.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'shop',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
+  { path: '', redirectTo: 'shop', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+
   {
     path: '',
     component: LandingComponent,
     children: [
-      {
-        path: 'shop',
-        component: ProductsComponentPage,
-      },
-      {
-        path: 'category/:id',
-        component: CategoryProductsComponent,
-      },
+      { path: 'shop', component: ProductsComponentPage },
+      { path: 'category/:id', component: CategoryProductsComponent },
     ],
   },
   {
     path: 'admin',
-    component: LayoutComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: '', redirectTo: 'admin-products', pathMatch: 'full' },
-      {
-        path: 'admin-products',
-        component: ProductsComponent,
-      },
-      {
-        path: 'categories',
-        component: CategoriesComponent,
-      },
-    ],
+    loadChildren: () =>
+      import('./pages/admin/admin.module').then((m) => m.AdminModule),
   },
 ];
---
+++ src/app/pages/admin/admin.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../../guards/auth.guard';
+import { LayoutComponent } from './layout/layout.component';
+import { ProductsComponent } from './products/products.component';
+import { CategoriesComponent } from './categories/categories.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', redirectTo: 'admin-products', pathMatch: 'full' },
+      { path: 'admin-products', component: ProductsComponent },
+      { path: 'categories', component: CategoriesComponent },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [LayoutComponent, ProductsComponent, CategoriesComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class AdminModule {}
